23c157b17552bfb80475a88e670c0522103f1733
snippet-softwares <snippetsoftwaresolutions@gmail.com>
2023-03-09 12:26:30 +0300

    first commit

---
+++ src/main/java/ke/co/proxyapi/mpesaencryption/InitiatorPasswordEncryption.java
@@ -0,0 +1,76 @@
+package ke.co.proxyapi.mpesaencryption;
+
+import javax.crypto.Cipher;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+
+public class InitiatorPasswordEncryption
+{
+    /**
+     * Encrypts plaintext password and returns encrypted password. No more changes are required to be done on
+     * the final text
+     *
+     * @param plain
+     * @return
+     * @throws Exception
+     */
+    protected String getEncryptedPasswd(String plain) throws Exception
+    {
+        try
+        {
+            InputStream is = new ByteArrayInputStream(getCertificate().getBytes());
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate) cf.generateCertificate(is);
+
+            PublicKey pubKey = cert.getPublicKey();
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+
+            byte cipherBytes[] =  cipher.doFinal(plain.getBytes());
+            String encrypted = Base64.getEncoder().encodeToString(cipherBytes);
+            return encrypted;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * The Public key certificate being used. Replace with the respective certificate required depending on environment.
+     * To get it, just open the certificate in notepad++ and copy paste the contents here, then remove the
+     * new line characters '\n' <strong>EXCEPT</strong> the first and last newlines. Check the one below for the required format
+     *
+     * This is currently the G2 sandbox certificate, will not work with Daraja sandbox
+     *
+     * @return certificate in String format
+     */
+    private String getCertificate()
+    {
+        String certStr = "-----BEGIN CERTIFICATE-----\n" +
+                "bWl0ZWQxEzARBgNVBAsTClRlY2hub2xvZ3kxKTAnBgNVBAMTIGFwaWdlZS5hcGlj" +
+                "YWxsZXIuc2FmYXJpY29tLmNvLmtlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIB" +
+                "CgKCAQEAoknIb5Tm1hxOVdFsOejAs6veAai32Zv442BLuOGkFKUeCUM2s0K8XEsU" +
+                "ZmFyaWNvbSUyMEludGVybmFsJTIwSXNzdWluZyUyMENBJTIwMDIsQ049U1ZEVDNJ" +
+                "U1NDQTAxLENOPUNEUCxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxDTj1TZXJ2" +
+                "aWNlcyxDTj1Db25maWd1cmF0aW9uLERDPXNhZmFyaWNvbSxEQz1uZXQ/Y2VydGlm" +
+                "aWNhdGVSZXZvY2F0aW9uTGlzdD9iYXNlP29iamVjdENsYXNzPWNSTERpc3RyaWJ1" +
+                "dGlvblBvaW50hkdodHRwOi8vY3JsLnNhZmFyaWNvbS5jby5rZS9TYWZhcmljb20l" +
+                "MjBJbnRlcm5hbCUyMElzc3VpbmclMjBDQSUyMDAyLmNybDCCAQkGCCsGAQUFBwEB" +
+                "BIH8MIH5MIHJBggrBgEFBQcwAoaBvGxkYXA6Ly8vQ049U2FmYXJpY29tJTIwSW50" +
+                "ZXJuYWwlMjBJc3N1aW5nJTIwQ0ElMjAwMixDTj1BSUEsQ049UHVibGljJTIwS2V5" +
+                "JTIwU2VydmljZXMsQ049U2VydmljZXMsQ049Q29uZmlndXJhdGlvbixEQz1zYWZh" +
+                "cmljb20sREM9bmV0P2NBQ2VydGlmaWNhdGU/YmFzZT9vYmplY3RDbGFzcz1jZXJ0" +
+                "aWZpY2F0aW9uQXV0aG9yaXR5MCsGCCsGAQUFBzABhh9odHRwOi8vY3JsLnNhZmFy" +
+                "aWNvbS5jby5rZS9vY3NwMAsGA1UdDwQEAwIFoDA9BgkrBgEEAYI3FQcEMDAuBiYr" +
+                "BgEEAYI3FQiHz4xWhMLEA4XphTaE3tENhqCICGeGwcdsg7m5awIBZAIBDDAdBgNV" +
+                "HSUEFjAUBggrBgEFBQcDAgYIKwYBBQUHAwEwJwYJKwYBBAGCNxUKBBowGDAKBggr" +
+                "-----END CERTIFICATE-----\n";
+        return certStr;
+    }
+}
---
+++ src/main/java/ke/co/proxyapi/mpesaencryption/Main.java
@@ -0,0 +1,26 @@
+package ke.co.proxyapi.mpesaencryption;
+
+public class Main
+{
+    public static void main(String args[]) throws Exception
+    {
+        //how to get initiator encrypted password
+        InitiatorPasswordEncryption app = new InitiatorPasswordEncryption();
+        String ciphertext = app.getEncryptedPasswd("APIUserPassword");
+        System.out.println("Encrypted password:\n" + ciphertext);
+        System.out.println();
+
+        //how to get API Authentication header
+//        ApiAuthenticationHeaderEncoding headerEncoding = new ApiAuthenticationHeaderEncoding();
+//        String headerEncodingEncoding = headerEncoding.getEncoded("mVjAxwuJd2xwtAFfaI6tjuffBNHCvVAi", "14HKuEbrj01psKRa");
+//        System.out.println("Auth Header: " + headerEncodingEncoding);
+//        System.out.println();
+
+        //how to encode STK Push/LnM credentials
+//        StkPushPasskeyEncoding passkeyEncoding = new StkPushPasskeyEncoding();
+//        StkPushPasskeyEncoding.StkPassword passkeyEncodingEncoded = passkeyEncoding.getEncoded(
+//                "174379",
+//                "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919");
+//        System.out.println("LnM Password: " + passkeyEncodingEncoded.getPassword() + ", " + passkeyEncodingEncoded.getTimestamp());
+    }
+}
